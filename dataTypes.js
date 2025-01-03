b8dd9bb9a2f05939218f1bb5981e0d967375bf6f
Parth Devani <parthdevani98@gmail.com>
2025-01-04 00:22:16 +0530

    Datatypes

---
+++ dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3)   // we are using nodejs, not browser
+
+console.log(3 + 3)
+
+console.log("Parth")
+
+let name = "Parth"
+let age = 20
+let isAdult = true
+let state = null
+
+
+//number = 2 to power 53
+//bigint
+//string -> " "
+//boolean -> true/false 
+//null -> standalone value
+//undefined -> standalone value  
+//symbol -> unique value
+
+
+console.log(typeof "Parth")
+console.log(typeof undefined)
+console.log(typeof null)    
