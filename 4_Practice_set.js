7be16aeaecff0727c21743c022595d6d9db450e2
Parth Devani <parthdevani98@gmail.com>
2025-01-04 13:10:31 +0530

    practice set

---
+++ 4_Practice_set.js
@@ -0,0 +1,30 @@
+// Chapter 1 - Q1
+let a = "Parth"
+let b = 6
+console.log(a + b) //Parth6
+
+// Chapter 1 - Q2
+console.log(typeof (a + b)) //string
+
+// Chapter 1 - Q3
+const a1 = {
+    name: "Parth",
+    section: 1,
+    isPrincipal: false
+}
+// a1 = 45 //error
+// a1 = {} //
+
+// Chapter 1 - Q4
+a1['friend'] = "Shubham"
+a1['name'] = "Lovish"
+console.log(a1)
+
+// Chapter 1 - Q5
+const dict = {
+    appreciate: "recognize the full worth of.",
+    ataraxia: "a state of freedom from emotional disturbance and anxiety",
+    yakka: "work, especially hard work."
+}
+
+console.log(dict['yakka'])
